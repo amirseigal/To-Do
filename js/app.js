1cfa2b2647411839547cd2c06028bf72d0bed6f1
amirseigal <amirreza592@example.com>
2026-02-02 21:25:22 +0100

    saves in localstorage

--- js/app.js
+++ js/app.js
@@ -2,9 +2,24 @@ const taskList = document.getElementById("task-list");
 const input = document.getElementById("task-name");
 const createBtn = document.getElementById("create-btn");
 
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+renderTasks();
+
 createBtn.addEventListener("click", function() {
     const taskName = input.value;
-    const list = document.createElement("li")
-    list.innerHTML = `${taskName}`
-    taskList.append(list)
+    tasks.push(taskName)
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    renderTasks();
+    input.value = "";
 })
+
+function renderTasks() {
+    taskList.textContent = "";
+    for(let i=0; i<tasks.length; i++){
+        const list = document.createElement("li");
+        list.textContent = `${tasks[i]}`;
+        taskList.append(list);
+};
+    
+    
+}
